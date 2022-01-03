3f45923ed3735131db52a014661214ecfbb718a6
Omar Valdivia Samudio <92412378+omarvalsam@users.noreply.github.com>
2022-01-03 00:11:11 -0700

    added code for both index, and genmarkdown files. as well as updated the readme to represent the output

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,104 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to return a message if user doesn't want contributors
+function renderContributingSection(confirmContributers, data) {
+  if (!confirmContributers) {
+    return `
+  Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+    `;
+  } else {
+    return `
+  ${data}
+    `;
+  }
+}
 
-// TODO: Create a function that returns the license link
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) { 
+  if (license !== 'no license') {
+    return `
+  ![badge](https://img.shields.io/badge/license-${license}-blue)
+    `;
+  } else {
+    return ' ';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+  return `
+  [${license}](https://choosealicense.com/licenses/${license})
+    `;
+  } else {
+    return ' ';
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// Function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+  return `
+  ## [License](#table-of-contents)
+  The application is covered under the following license:
+  ${renderLicenseLink(license)}
+    `;
+  } else {
+    return ' ';
+  }
+ }
+
+ // Function that returns license in table of contents
+ // If there is no license, return an empty string
+function renderLicenseTOC(license) {
+  if (license !== 'no license') {
+  return `
+  * [License](#license)
+    `;
+  } else {
+    return ' ';
+  }
+ }
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+  ## Table-of-Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseTOC(data.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## [Description](#table-of-contents)
+  ${data.what}
+  ${data.why}
+  ${data.how}
+  ## [Installation](#table-of-contents)
+  ${data.installation}
+  ## [Usage](#table-of-contents)
+  ${data.usage}
+  
+  For more information on how to add screenshots for examples, visit the following website:
+  
+  
+  
+  ${renderLicenseSection(data.license)}
+  ## [Contributing](#table-of-contents)
+  
+  ${renderContributingSection(data.confirmContributers, data.contribute)}
+  ## [Tests](#table-of-contents)
+  ${data.test}
+  ## [Questions](#table-of-contents)
+  Please contact me using the following links:
+  [GitHub](https://github.com/${data.githubUsername})
+  [Email: ${data.email}](mailto:${data.email})
 `;
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](./readme-guide.md)
+  # This project to create read Me's!!
+  
+   
+  ## Table-of-Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+   
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## [Description](#table-of-contents)
+  This project is meant to make readme files easier to make in the long run, primarly focusing on students who need to focus more on the project than the written portion of a file.
+  Due to the fast paced enviroment we have in this class its diffuclt to keep up, this program should assist me in the future.
+  by refrencing my links in this readme or going to my github
+  ## [Installation](#table-of-contents)
+  Go ahead and create an empty folder to hold on to this program, then clone it from my repo. Afterwords go ahead and type npm install to inital all your files you need, then go ahead and start the document to further assist with this program. 
+  ## [Usage](#table-of-contents)
+  please go to the link provided to watch a video tutorial:
+  
+  For more information on how to add screenshots for examples, visit the following website:
+  
+  
+  
+   
+  ## [Contributing](#table-of-contents)
+  
+  
+  Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+    
+  ## [Tests](#table-of-contents)
+  just go ahead and start the command code from vscode and then type " node index" this should initialize the program and prompt you with questions. Afterward it should create a Readme for you to copy over to other programs. 
+  ## [Questions](#table-of-contents)
+  Please contact me using the following links:
+  [GitHub](https://github.com/omarvalsam)
+  [Email: omarvalsam@gmail.com](mailto:omarvalsam@gmail.com)
